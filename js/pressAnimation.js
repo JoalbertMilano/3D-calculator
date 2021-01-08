79b382ba7d79744403680d1b4846da8e11b55d64
JoalbertMilano <joalbertmilano@gmail.com>
2021-01-07 20:19:51 -0400

    Añadiendo animacion al presionar los botones

--- js/pressAnimation.js
+++ js/pressAnimation.js
@@ -1,8 +1,16 @@
 const botones = document.querySelectorAll(".btn");
 
 for(let boton of botones){
-    console.log(boton);
+    let borderLeft = boton.firstElementChild;
+    let borderBoton = boton.lastElementChild;
     boton.addEventListener("click", ()=>{
-        
+        boton.classList.add("btn-active");
+        borderLeft.classList.add("btn-brd-l-active");
+        borderBoton.classList.add("btn-brd-b-active");
+    });
+    boton.addEventListener("webkitAnimationEnd", ()=>{
+        boton.classList.remove("btn-active");
+        borderLeft.classList.remove("btn-brd-l-active");
+        borderBoton.classList.remove("btn-brd-b-active");
     });
 }
